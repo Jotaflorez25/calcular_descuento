917482f967b294b5c0cf96a798c4ad2db0d61600
Jotaflorez25 <109303761+Jotaflorez25@users.noreply.github.com>
2025-01-25 13:26:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -69,10 +69,7 @@ function calculoPrecio() {
     const precioFinal = (precioConDescuento * (1 + (ivaPre / 100))) + impoConsu;
 
     // Formatear el precio final con separadores de miles y punto decimal
-    const precioFinalFormateado = new Intl.NumberFormat('es-ES', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(precioFinal);
+    const precioFinalFormateado = new Intl.NumberFormat('eu-EU').format(precioFinal);
 
     document.getElementById('resultadoPrecio').textContent = `El precio final es: ${precioFinalFormateado}`;
 }
